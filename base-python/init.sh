d3a2f1ee7d8d397705552556a61a6174ea85b848
Max Kuznetsov <maks.kuznetsov@gmail.com>
2020-05-15 22:52:21 +0300

    add base-python

---
+++ base-python/init.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/dumb-init /bin/bash
+
+uid="$(id -u)"
+
+if [ "${uid}" -eq 0 ]; then
+  # set container's time zone
+  set-timezone
+  chown -R "$APP_LOGIN:$APP_LOGIN" /srv
+fi
+
+if [[ -f "/srv/init.sh" ]]; then
+  echo "starting /srv/init.sh"
+  chmod +x /srv/init.sh
+
+  if /srv/init.sh; then
+    echo "/srv/init.sh failed"
+    exit 1
+  fi
+fi
+
+if [[ $# -eq 0 ]]; then
+  echo "no arguments: starting root shell"
+  exec /bin/bash
+fi
+
+echo "starting $*"
+if [ "${uid}" -eq "0" ]; then
+  su-exec "$APP_LOGIN" "$@"
+else
+  exec "$@"
+fi
