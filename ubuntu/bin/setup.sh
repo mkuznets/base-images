03ab808399bde2c0ad4f4f658be851bc4c5c0294
Max Kuznetsov <maks.kuznetsov@gmail.com>
2020-11-04 22:23:23 +0300

    support multiple ubuntu releases

---
+++ ubuntu/bin/setup.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -xe
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update -q
+apt-get install --yes --quiet --no-upgrade --no-install-recommends \
+  locales \
+  tzdata \
+  make \
+  tcpdump \
+  wget \
+  curl \
+  less \
+  git \
+  vim-tiny \
+  ca-certificates
+
+locale-gen --purge en_US.UTF-8
+ln -s /usr/bin/vi /usr/local/bin/vim
+
+# ------------------------------------------------------------------------------
+# dumb-init
+
+CHECKSUM="
+45b1bbf56cc03edda81e4220535a025bfe3ed6e93562222b9be4471005b3eeb3 *dumb-init_1.2.2_aarch64
+37f2c1f0372a45554f1b89924fbb134fc24c3756efaedf11e07f599494e0eff9 *dumb-init_1.2.2_amd64
+45b1bbf56cc03edda81e4220535a025bfe3ed6e93562222b9be4471005b3eeb3 *dumb-init_1.2.2_arm64
+88b02a3bd014e4c30d8d54389597adc4f5a36d1d6b49200b5a4f6a71026c2246 *dumb-init_1.2.2_ppc64el
+88b02a3bd014e4c30d8d54389597adc4f5a36d1d6b49200b5a4f6a71026c2246 *dumb-init_1.2.2_ppc64le
+8b3808c3c06d008b8f2eeb2789c7c99e0450b678d94fb50fd446b8f6a22e3a9d *dumb-init_1.2.2_s390x
+37f2c1f0372a45554f1b89924fbb134fc24c3756efaedf11e07f599494e0eff9 *dumb-init_1.2.2_x86_64
+"
+
+ARCH="$(uname -m)"
+
+wget "https://github.com/Yelp/dumb-init/releases/download/v1.2.2/dumb-init_1.2.2_$ARCH"
+echo "$CHECKSUM" | grep -e "${ARCH}$" | sha256sum -c -
+mv "dumb-init_1.2.2_$ARCH" /usr/local/bin/dumb-init
+
+chmod +x /usr/local/bin/dumb-init
+
+# ------------------------------------------------------------------------------
+# Cleanup
+
+apt-get clean
+rm -rf /var/lib/apt/lists/*
